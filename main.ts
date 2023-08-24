7dd88fcddbee8955f05fbbb06f77d7c065c58ae1
ShacharHarshuv <shachar.harshuv@gmail.com>
2023-08-24 16:38:15 -0400

    Use only editorCallback as per feedback

--- main.ts
+++ main.ts
@@ -1,36 +1,27 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-const checkboxRegex = /((?:\n|^)\s*)- \[.\]/g;
+import {
+  Editor,
+  MarkdownView,
+  Plugin,
+} from 'obsidian';
 
 export default class UncheckAllPlugin extends Plugin {
-  private _uncheckAll() {
-    const activeLeaf = this.app.workspace.activeLeaf;
-    if (activeLeaf?.view instanceof MarkdownView) {
-      const editor = activeLeaf.view.editor;
-      const doc = editor.getDoc();
-      const cursorPosition = doc.getCursor();
-      const entireContent = doc.getValue();
-      const updatedContent = entireContent.replace(checkboxRegex, "$1- [ ]");
-      doc.setValue(updatedContent);
-      doc.setCursor(cursorPosition);
-    }
+  async onload() {
+    // This adds a simple command that can be triggered anywhere
+    this.addCommand({
+      id: 'uncheck-all',
+      name: 'Uncheck All Checkboxes in File',
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        const doc = editor.getDoc();
+        const cursorPosition = doc.getCursor();
+        const entireContent = doc.getValue();
+        const updatedContent = entireContent.replace(/((?:\n|^)\s*)- \[.\]/g, "$1- [ ]");
+        doc.setValue(updatedContent);
+        doc.setCursor(cursorPosition);
+      },
+    });
   }
 
-	async onload() {
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'uncheck-all',
-			name: 'Uncheck All Checkboxes in File',
-			callback: () => {
-        this._uncheckAll();
-			},
-      editorCallback: (editor: Editor, view: MarkdownView) => {
-        this._uncheckAll();
-      },
-		});
-	}
+  onunload() {
 
-	onunload() {
-
-	}
+  }
 }
